6d867830349dfc3a9a8e7c0379ecb7eaedfa8831
Kaleshwar Lakaram <154596529+kaleshwar-2005@users.noreply.github.com>
2025-11-23 09:58:36 +0530

    Set PORT to 5000 instead of environment variable

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const { body, validationResult } = require('express-validator');
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
